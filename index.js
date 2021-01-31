71cd35ce0a88f798b08a27c62afbb089698d19c9
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-31 05:39:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -47,10 +47,10 @@ function  indexAt(node,collection,linkedList){
 
 
 function insertNodeAt(index,newLinkedList,linkedList,collection){
-  // for(let i=0; i<index ;i++){
+   for(let i=0; i<index ;i++){
     // if(collection[linkedList] == linkedList){
         collection[newLinkedList].next = linkedList
-  //   }
+     }
     //  linkedList = collection[linkedList].next
    //}
 }
