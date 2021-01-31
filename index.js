f496391d5d26116519f83336f7e7f51efdb9e0a0
AHMED AFANDY <eng.computer1@yahoo.com>
2021-01-31 03:10:42 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -19,13 +19,13 @@ function next(node,collection){
 
 
 function nodeAt(index,node,collection){
-      //for(let i=0; i < index;i++){
+      for(let i=0; i < index;i++){
 
         node=next(node,collection)
           if(node == undefined){
              return collection[node]
           }
-    //  }
+      }
 
 
 }
